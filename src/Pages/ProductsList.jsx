23ee79c444a8b16a0c840e0a1efd2fc7fa78d880
betancesdar <97894736+betancesdar@users.noreply.github.com>
2022-07-05 20:37:39 -0400

    ProductList

---
+++ src/Pages/ProductsList.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+import Navbar from "../Components/Navbar"
+import Ads from "../Components/Ads"
+
+const Container = styled.div`
+
+`;
+
+const FilterContainer = styled.div`
+
+`;
+
+
+const ProductsList = () => {
+  return (
+    <Container>
+      <Navbar/>
+      <Ads/>
+      <FilterContainer>
+
+
+      </FilterContainer>
+
+    </Container>
+  )
+}
+
+export default ProductsList
